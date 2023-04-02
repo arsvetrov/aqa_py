5796edd04ec1ed28fe01ff268e1e99954faeac5f
OPanchenko <pan.pan.4it@gmail.com>
2023-04-02 10:57:29 +0300

    lession 08 testing - update

--- lession_08/test_08_code.py
+++ lession_08/test_08_code.py
@@ -1,9 +1,16 @@
 import unittest
+#import sys
+#import pathlib
+#sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
+from lession_06 import lession_06_code
+
 
 class HomeworksTesting(unittest.TestCase):
 
     def test_task01(self):
-        pass
+        actual_result = lession_06_code.square(2)
+        expected_result = 4
+        self.assertEqual(actual_result, expected_result)
 
 if __name__ == "__main__":
     unittest.main()
