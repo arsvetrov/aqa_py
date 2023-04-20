c8c9e06f00cb6035609e70e6eaf543edcdacf512
OPanchenko <pan.pan.4it@gmail.com>
2023-04-20 09:10:48 +0300

    Lession 10 & hw & test

--- lession_10/test_10.py
+++ lession_10/test_10.py
@@ -1,9 +1,13 @@
 import unittest
 from lession_10_code import *
-
+from my_logger import logger
+# Тестування виключень
 class TestDivide(unittest.TestCase):
     def test_divide_by_zero(self):
-        self.assertRaises(ZeroDivisionError, calculate_sum, 10, 0)
+        "test for raise exception"
+        logger.info("create exception")
+        #self.assertRaises(ZeroDivisionError, calculate_sum, 10, 0)
+        self.assertRaises(ZeroDivisionError, divide, 10, 0)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
